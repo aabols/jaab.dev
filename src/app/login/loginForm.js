b39345213a618d748b94a436e13c1075025a34c7
jaab <trakaisjanka@gmail.com>
2024-08-26 23:31:13 +0100

    refactor login form button

--- src/app/login/loginForm.js
+++ src/app/login/loginForm.js
@@ -1,33 +1,15 @@
+'use client'
 import React from 'react'
 import FormField from './formField'
-import clsx from 'clsx'
+import FormButton from './formButton'
+import { loginUser } from './actions'
 
 export default function LoginForm() {
   return (
     <form className='max-w-sm mx-auto'>
       <FormField id='username' type='text' label='Username' />
       <FormField id='password' type='password' label='Password' />
-
-      <button
-        type='submit'
-        className={clsx([
-          'bg-amber-700',
-          'hover:bg-amber-800',
-          'w-full',
-          'sm:w-auto',
-          'px-5',
-          'py-2.5',
-          'rounded',
-          'text-white',
-          'text-center',
-          'text-sm',
-          'font-medium',
-          'dark:bg-amber-600',
-          'dark:hover:bg-amber-700',
-        ])}
-      >
-        Log in
-      </button>
+      <FormButton caption='Log in' pendingCaption='Logging in...' />
     </form>
   )
 }
