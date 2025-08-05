15080c85a39721779d081e94c38f514b94e1a772
jaab <trakaisjanka@gmail.com>
2025-08-05 22:37:33 +0100

    dedicated config file for sequelize-cli

---
+++ sequelize-cli-config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+require('dotenv').config();
+
+console.log('t', process.env.SERVER_ROOT)
+
+// CONFIGURATION
+const config = {
+  db: {
+    dialect: process.env.DB_DIALECT || 'sqlite',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_NAME || 'dev-db',
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || 'rootpw',
+    seederStorage: 'sequelize',
+    storage: process.env.DB_STORAGE || path.join(process.env.SERVER_ROOT, 'db/dev-db.sqlite3')
+  },
+}
+
+module.exports = config
